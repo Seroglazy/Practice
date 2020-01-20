6e5d27027f489cd221a9899ff2b4e8aeadd22325
Seroglazy <57675955+Seroglazy@users.noreply.github.com>
2020-01-20 15:06:42 +0300

    Add files via upload

---
+++ Championship/Form1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading;
+using System.Windows.Forms;
+using System.Net; 
+
+namespace Championship
+{
+
+public partial class Form1 : Form
+{
+HttpListener server;
+bool flag = true;
+
+    public Form1()
+    {
+        InitializeComponent();
+    }
+
+    private void button1_Click(object sender, EventArgs e)
+    {
+//запись адреса и порта
+        string uri = @"http://+:8080/";
+        StartServer(uri);
+    }
+
+    private void StartServer(string prefix)
+    {
+        server = new HttpListener();
+// проверка совместимости с HttpListener 
+        if (!HttpListener.IsSupported) return;
+        if (string.IsNullOrEmpty(prefix))
+        throw new ArgumentException("prefix");
+        server.Prefixes.Add(prefix);
+
+//запуск сервера
+        server.Start();
+        this.Text = "Сервер запущен!";
+        while (server.IsListening)
+        {
+//ожидание входящих запросов
+            HttpListenerContext context = server.GetContext();
+//получение запросов
+            HttpListenerRequest request = context.Request;
+            if (request.HttpMethod == "POST")
+            {
+            ShowRequestData(request);
+            if (!flag) return;
+            }
+//формирование ответа сервера:
+            string responseString = @"<!DOCTYPE HTML>
+            <html><head></head><body>
+            <form method=""post"" action=""say"">
+            <p><b>Name: </b><br>
+            <input type=""text"" name=""myname"" size=""40""></p>
+            <p><input type=""submit"" value=""send""></p>
+            </form></body></html>";
+//отправка данных клиенту
+            HttpListenerResponse response = context.Response;
+            response.ContentType = "text/html; charset=UTF-8";
+            byte[] buffer = Encoding.UTF8.GetBytes(responseString);
+            response.ContentLength64 = buffer.Length;
+            using (Stream output = response.OutputStream)
+            {
+            output.Write(buffer, 0, buffer.Length);
+            }
+        }
+    }
+
+    private void ShowRequestData(HttpListenerRequest request)
+    {
+//проверка получения данных
+    if (!request.HasEntityBody) return;
+    using (Stream body = request.InputStream)
+        {
+        using (StreamReader reader = new StreamReader(body))
+        {
+            string text = reader.ReadToEnd();
+            text = text.Remove(0, 7);
+//преобразование полученной строки
+            text = WebUtility.UrlDecode(text);
+//вывод полученного ответа
+
+//остановка сервера по команде stop с клиента
+            if (text == "stop")
+            {
+                server.Stop();
+                this.Text = "Сервер остановлен!";
+                flag = false;
+            }
+            else
+            {
+                MessageBox.Show("Ваше имя: " + text);
+                flag = true;
+            }
+        }   
+        }
+    }
+}
+}
